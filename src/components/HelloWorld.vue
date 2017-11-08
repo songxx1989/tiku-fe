f65d82fd2e313e28ead89b13487b924c847e02dd
song <songxx1989@foxmail.com>
2017-11-08 18:56:30 +0800

    新增题目列表和表单

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,47 +1,71 @@
 <template>
-  <el-table
-    :data="tableData"
-    style="width: 100%">
-    <el-table-column
-      prop="date"
-      label="日期"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="姓名"
-      width="180">
-    </el-table-column>
-    <el-table-column
-      prop="address"
-      label="地址">
-    </el-table-column>
-  </el-table>
+  <div>
+    <el-form ref="form" :model="form" label-width="80px">
+
+      <el-form-item label="题目分类">
+        <el-select v-model="form.cid" placeholder="请选择题目分类">
+          <el-option label="分类1" value="shanghai"></el-option>
+          <el-option label="分类2" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="题目难度">
+        <el-select v-model="form.lid" placeholder="请选择题目难度">
+          <el-option label="难度1" value="shanghai"></el-option>
+          <el-option label="难度2" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="题干">
+        <el-input type="textarea" v-model="form.text"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">立即创建</el-button>
+        <el-button>取消</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-table
+      :data="tableData">
+      <el-table-column
+        prop="id"
+        label="编号">
+      </el-table-column>
+      <el-table-column
+        prop="cid"
+        label="分类">
+      </el-table-column>
+      <el-table-column
+        prop="lid"
+        label="难度">
+      </el-table-column>
+      <el-table-column
+        prop="text"
+        label="题目">
+      </el-table-column>
+    </el-table>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      tableData: [{
-        date: '2016-05-02',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1518 弄'
-      }, {
-        date: '2016-05-04',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1517 弄'
-      }, {
-        date: '2016-05-01',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1519 弄'
-      }, {
-        date: '2016-05-03',
-        name: '王小虎',
-        address: '上海市普陀区金沙江路 1516 弄'
-      }]
+  import axios from 'axios'
+
+  export default {
+    name: 'HelloWorld',
+    created () {
+      let self = this
+      axios.get('http://localhost:8080/ques')
+        .then(resp => self.tableData = resp.data)
+        .catch(err => console.log(err))
+    },
+    data () {
+      return {
+        tableData: [],
+        form: {cid: '', lid: '', text: ''}
+      }
+    },
+    methods: {
+      onSubmit () {
+        console.log('submit!')
+      }
     }
   }
-}
 </script>
